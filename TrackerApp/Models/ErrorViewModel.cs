2ced70d719ca9b9bf75168979696eb8bbbaac62d
Arvinth Kumar.S <arvinthkumar.s@gmail.com>
2021-05-12 22:56:08 +0530

    Model and ConnectionString added

--- TrackerApp/Models/ErrorViewModel.cs
+++ TrackerApp/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace TrackerApp.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+         
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
