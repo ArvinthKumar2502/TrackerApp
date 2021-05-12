ccc429fe05b0388e09f75f666eacd11691be100a
Arvinth Kumar.S <arvinthkumar.s@gmail.com>
2021-05-12 23:15:50 +0530

    DbContext Setup

---
+++ TrackerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrackerApp.Models;
+
+namespace TrackerApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
