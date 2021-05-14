01b62535aaff3ddeea558f4a7a614802cb403d84
Arvinth Kumar.S <arvinthkumar.s@gmail.com>
2021-05-14 15:26:18 +0530

    Item Model updated
    Added itemName and Lender attributes

--- TrackerApp/Models/Item.cs
+++ TrackerApp/Models/Item.cs
@@ -11,5 +11,8 @@ namespace TrackerApp.Models
         [Key]
         public int Id { get; set; }
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
+
     }
 }
